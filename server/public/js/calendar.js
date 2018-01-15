68dc0c8ea93abbc367baaa88db65167d1e80e4f5
John Henning <jlhbaseball15@me.com>
2018-01-15 14:47:48 -0500

    initial refactor for other schools (#17)
    * beginning of refactor to abstract schools to specific API
    
    * formatted
    
    * see if it works on heroku
    
    * fixed typo
    
    * added new models

--- server/public/js/calendar.js
+++ server/public/js/calendar.js
@@ -1,39 +1,43 @@
+class calendar {
+  constructor(divToBindTo, calendarNumber) {
+    this.divToBindTo = divToBindTo;
+    this.boxArr = {};
+    this.calendarNumber = calendarNumber;
+  }
+  addSection(options) {
+    //Create elements
+    var tempArr = [];
+    var self = this;
+    for (var i = 0; i < options.sectionMeetTimes.length; i++) {
+      for (var j = 0; j < options.sectionMeetTimes[i].meetDays.length; j++) {
+        var sectionMeetTimey = new sectionMeetTime(
+          this.days[options.sectionMeetTimes[i].meetDays[j]]
+        );
+        sectionMeetTimey.addSectionName(options.section);
+        sectionMeetTimey.addStartStop(
+          convertToNum(options.sectionMeetTimes[i].meetPeriodBegin),
+          convertToNum(options.sectionMeetTimes[i].meetPeriodEnd)
+        );
 
-class calendar{
-	constructor(divToBindTo, calendarNumber){
-		this.divToBindTo = divToBindTo;
-		this.boxArr = {};
-		this.calendarNumber = calendarNumber;
-	}
-	addSection(options){
-		//Create elements
-		var tempArr=[];
-		var self =this;
-		for(var i=0; i < options.sectionMeetTimes.length; i++){
-			for(var j =0; j< options.sectionMeetTimes[i].meetDays.length; j++){
-				var sectionMeetTimey = new sectionMeetTime(this.days[options.sectionMeetTimes[i].meetDays[j]]);
-				sectionMeetTimey.addSectionName(options.section);
-				sectionMeetTimey.addStartStop(convertToNum(options.sectionMeetTimes[i].meetPeriodBegin), convertToNum(options.sectionMeetTimes[i].meetPeriodEnd));
-				
-				sectionMeetTimey.addColor(options.color);
-				sectionMeetTimey.render();
-				var info = "swag";
-				sectionMeetTimey.addSideBarHandler(self.toggleSideBar, options, self);
-				tempArr.push(sectionMeetTimey);
-			}
-		}
-		this.boxArr[options.section] = tempArr;
-	}
+        sectionMeetTimey.addColor(options.color);
+        sectionMeetTimey.render();
+        var info = "swag";
+        sectionMeetTimey.addSideBarHandler(self.toggleSideBar, options, self);
+        tempArr.push(sectionMeetTimey);
+      }
+    }
+    this.boxArr[options.section] = tempArr;
+  }
 
-	removeSection(section){
-		this.boxArr[section].deleteMe();
-	}
-	addSideBar(){
-		return $(`<div class="ui sidebar inverted vertical "></div>`);
-	}
-	toggleSideBar(info, self){
-		self.sideBar.empty();
-		self.sideBar.append(`
+  removeSection(section) {
+    this.boxArr[section].deleteMe();
+  }
+  addSideBar() {
+    return $(`<div class="ui sidebar inverted vertical "></div>`);
+  }
+  toggleSideBar(info, self) {
+    self.sideBar.empty();
+    self.sideBar.append(`
 			<div class="ui container">
 			<h3 class="ui header">${info.code}</h3>
 			<h2 class="ui sub header"><i>${info.name}</i></h2> 
@@ -55,98 +59,156 @@ class calendar{
 				</div>
 			 </div>
 		  </div>`);
-		self.sideBar
-		  .sidebar({
-		    context: self.calendarDiv
-		  })
-		  .sidebar('toggle');
-	}
-	addDayColumns(innerElement){
-		this.days = {}; 
-		this.days["M"]=$(`<div class="two wide column"><div style="overflow:hidden;">Monday</div></div>`);
-		this.days["T"]=$(`<div class="two wide column"><div style="overflow:hidden;">Tuesday</div></div>`);
-		this.days["W"]=$(`<div class="two wide column"><div style="overflow:hidden;">Wednesday</div></div>`);
-		this.days["R"]=$(`<div class="two wide column"><div style="overflow:hidden;">Thursday</div></div>`);
-		this.days["F"]=$(`<div class="two wide column"><div style="overflow:hidden;">Friday</div></div>`);
-		this.days["S"]=$(`<div class="two wide column"><div style="overflow:hidden;">Saturday</div></div>`);
-		this.days["U"]=$(`<div class="two wide column"><div style="overflow:hidden;">Sunday</div></div>`);
-		var keys = Object.keys(this.days);
+    self.sideBar
+      .sidebar({
+        context: self.calendarDiv
+      })
+      .sidebar("toggle");
+  }
+  addDayColumns(innerElement) {
+    this.days = {};
+    this.days["M"] = $(
+      `<div class="two wide column"><div style="overflow:hidden;">Monday</div></div>`
+    );
+    this.days["T"] = $(
+      `<div class="two wide column"><div style="overflow:hidden;">Tuesday</div></div>`
+    );
+    this.days["W"] = $(
+      `<div class="two wide column"><div style="overflow:hidden;">Wednesday</div></div>`
+    );
+    this.days["R"] = $(
+      `<div class="two wide column"><div style="overflow:hidden;">Thursday</div></div>`
+    );
+    this.days["F"] = $(
+      `<div class="two wide column"><div style="overflow:hidden;">Friday</div></div>`
+    );
+    this.days["S"] = $(
+      `<div class="two wide column"><div style="overflow:hidden;">Saturday</div></div>`
+    );
+    this.days["U"] = $(
+      `<div class="two wide column"><div style="overflow:hidden;">Sunday</div></div>`
+    );
+    var keys = Object.keys(this.days);
 
-        innerElement.append(`<div class="two wide column"></div>`);
-        for(var i = 0; i< keys.length; i++){
-        	innerElement.append(this.days[keys[i]]);
-        }
-        
-	}
-	addTimes(periods){
-		 var text = '';
-		var times=[" (7:25 - 8:15)", " (8:30 - 9:20)", " (9:35 - 10:25)", " (10:40 - 11:30)", " (11:45 - 12:35)", " (12:50 - 1:40)", " (1:55 - 2:45)", " (3:00 - 3:50)", " (4:05 - 4:55)", " (5:10 - 6:00)", " (6:15 - 7:05)", " (7:20 - 8:10)", " (8:20 - 9:10)", " (9:20 - 10:10)" ];
-    	for(var i = 0; i < periods.length; i++){
-			text += `<div style="height:20px; margin-bottom:20px; overflow:hidden; position:relative;"> <h4>${periods[i]}<span class="thin">${times[i]}</span></h4> </div>`;
-		}
-        return $(`<div class="two wide column">${text}</div>`);
-	}
-	addDividers(periods){
-		var text = "";
-        for(var i = 0; i < periods.length; i++){
-			text += `<div style="top: ${i*24+40}px;     width: 100%; position:relative;"><div class="ui  divider"></div></div>`;
-		}
-		return $(`<div style="position: absolute;  width:100%;">${text}</div>`);
-	}
-	addBaseHtml(){
+    innerElement.append(`<div class="two wide column"></div>`);
+    for (var i = 0; i < keys.length; i++) {
+      innerElement.append(this.days[keys[i]]);
+    }
+  }
+  addTimes(periods) {
+    var text = "";
+    var times = [
+      " (7:25 - 8:15)",
+      " (8:30 - 9:20)",
+      " (9:35 - 10:25)",
+      " (10:40 - 11:30)",
+      " (11:45 - 12:35)",
+      " (12:50 - 1:40)",
+      " (1:55 - 2:45)",
+      " (3:00 - 3:50)",
+      " (4:05 - 4:55)",
+      " (5:10 - 6:00)",
+      " (6:15 - 7:05)",
+      " (7:20 - 8:10)",
+      " (8:20 - 9:10)",
+      " (9:20 - 10:10)"
+    ];
+    for (var i = 0; i < periods.length; i++) {
+      text += `<div style="height:20px; margin-bottom:20px; overflow:hidden; position:relative;"> <h4>${
+        periods[i]
+      }<span class="thin">${times[i]}</span></h4> </div>`;
+    }
+    return $(`<div class="two wide column">${text}</div>`);
+  }
+  addDividers(periods) {
+    var text = "";
+    for (var i = 0; i < periods.length; i++) {
+      text += `<div style="top: ${i * 24 +
+        40}px;     width: 100%; position:relative;"><div class="ui  divider"></div></div>`;
+    }
+    return $(`<div style="position: absolute;  width:100%;">${text}</div>`);
+  }
+  addBaseHtml() {
+    var element = $(`<div  class="pushable"> </div>`);
+    var pusher = $(
+      `<div  style="background-color:#2196f3; " class="pusher"> </div>`
+    );
+    var innerElement = $(`<div  class="ui grid " ></div>`);
 
-		var element = $(`<div  class="pushable"> </div>`);
-		var pusher = $(`<div  style="background-color:#2196f3; " class="pusher"> </div>`);
-        var innerElement = $(`<div  class="ui grid " ></div>`);
-        
-		//Add day columns
-		this.addDayColumns(innerElement);
-		var periods=["1","2","3","4","5","6","7","8","9","10","11","E1","E2","E3"];
+    //Add day columns
+    this.addDayColumns(innerElement);
+    var periods = [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "E1",
+      "E2",
+      "E3"
+    ];
 
-        innerElement.append(this.addTimes(periods));
-        innerElement.append(this.addDividers(periods));
-     	this.sideBar = this.addSideBar();
-     	element.append(this.sideBar);
+    innerElement.append(this.addTimes(periods));
+    innerElement.append(this.addDividers(periods));
+    this.sideBar = this.addSideBar();
+    element.append(this.sideBar);
 
-        //add Schedule name and attach every thing
-        pusher.append(`<h2 style="width:100%; text-align:center;">Option: ${this.calendarNumber+1}</h2>`);
-    	pusher.append(innerElement);
-    	element.append(pusher);
-		this.calendarDiv = element;
-		$("#" + this.divToBindTo).append(element);
-	}
-	deleteMe(){
-		this.calendarDiv.remove();
-		delete this;
-	}
+    //add Schedule name and attach every thing
+    pusher.append(
+      `<h2 style="width:100%; text-align:center;">Option: ${this
+        .calendarNumber + 1}</h2>`
+    );
+    pusher.append(innerElement);
+    element.append(pusher);
+    this.calendarDiv = element;
+    $("#" + this.divToBindTo).append(element);
+  }
+  deleteMe() {
+    this.calendarDiv.remove();
+    delete this;
+  }
 }
-class sectionMeetTime{
-	constructor(divToBindTo){
-		this.divToBindTo = divToBindTo;
-		this.element = $(`<div style=" width:100%; z-index:3;" class="ui middle aligned centered grid" ></div>`);
-	}
-	render(){
-		this.divToBindTo.append(this.element);
-	}
-	addStartStop(start, stop){
-		this.element.css({"position":"absolute", "margin-top":"0", "top":`${((start*40)+14)}px`, "height":`${((stop-start)+1)*40}px`});
-	}
-	addSectionName(sectionName){
-		this.element.append(`<div class="column" style="padding:0px;"><h3 style="text-align:center; width:100%; ">${sectionName}</h3></div>`);
-	}
-	deleteMe(){
-		this.element.remove();
-	}
-	addSideBarHandler(callback, info, self){
-		var x = callback;
-		var box = this;
-		this.element.click(function(){
-			box.element.transition('pulse');
-			x(info, self);
-		});
-	}
-	addColor(color){
-		
-		this.element.css("background-color", color);
-	}
+class sectionMeetTime {
+  constructor(divToBindTo) {
+    this.divToBindTo = divToBindTo;
+    this.element = $(
+      `<div style=" width:100%; z-index:3;" class="ui middle aligned centered grid" ></div>`
+    );
+  }
+  render() {
+    this.divToBindTo.append(this.element);
+  }
+  addStartStop(start, stop) {
+    this.element.css({
+      position: "absolute",
+      "margin-top": "0",
+      top: `${start * 40 + 14}px`,
+      height: `${(stop - start + 1) * 40}px`
+    });
+  }
+  addSectionName(sectionName) {
+    this.element.append(
+      `<div class="column" style="padding:0px;"><h3 style="text-align:center; width:100%; ">${sectionName}</h3></div>`
+    );
+  }
+  deleteMe() {
+    this.element.remove();
+  }
+  addSideBarHandler(callback, info, self) {
+    var x = callback;
+    var box = this;
+    this.element.click(function() {
+      box.element.transition("pulse");
+      x(info, self);
+    });
+  }
+  addColor(color) {
+    this.element.css("background-color", color);
+  }
 }
--- server/public/js/search.js
+++ server/public/js/search.js
@@ -1,115 +1,113 @@
-class addNewSearchButton{
-	constructor(divToBindTo){
-		this.divToBindTo = divToBindTo;
-		this.addHtml();
-		this.render();
-	}
-	addHtml(){
-		this.element = $(`<button class="ui basic icon button" data-tooltip="Add more courses" data-inverted="">
+class addNewSearchButton {
+  constructor(divToBindTo) {
+    this.divToBindTo = divToBindTo;
+    this.addHtml();
+    this.render();
+  }
+  addHtml() {
+    this.element = $(`<button class="ui basic icon button" data-tooltip="Add more courses" data-inverted="">
   			<i class="add icon"></i>
 			</button>`);
-	}
-	attachClickHandler(callback){
-		this.element.on('click', function(){
-			callback();
-		});
-	}
-	render(){
-		$('#' + this.divToBindTo).append(this.element);
-	}
+  }
+  attachClickHandler(callback) {
+    this.element.on("click", function() {
+      callback();
+    });
+  }
+  render() {
+    $("#" + this.divToBindTo).append(this.element);
+  }
 }
-class SearchBox{
-	constructor(divToBindTo){
-		//Order matters
-		this.divToBindTo = divToBindTo;
-		this.active=false;
-		this.addHtml();
-		this.attachKeydownHandler();
-		this.render();
-	}
-	addHtml(){
-		this.element = $(`<div class="ui inverted card"></div>`);
-      	var innerHtml =$(`<div class="ui icon input "></div>`);
-        this.searchBox = $(`<input type="text" placeholder="Enter Course">`);	
-        var searchIcon = $(`<i class="search icon "></i>`);
-      	innerHtml.append(this.searchBox);
-      	innerHtml.append(searchIcon);
-      	this.element.append(innerHtml);
+class SearchBox {
+  constructor(divToBindTo) {
+    //Order matters
+    this.divToBindTo = divToBindTo;
+    this.active = false;
+    this.addHtml();
+    this.attachKeydownHandler();
+    this.render();
+  }
+  addHtml() {
+    this.element = $(`<div class="ui inverted card"></div>`);
+    var innerHtml = $(`<div class="ui icon input "></div>`);
+    this.searchBox = $(`<input type="text" placeholder="Enter Course">`);
+    var searchIcon = $(`<i class="search icon "></i>`);
+    innerHtml.append(this.searchBox);
+    innerHtml.append(searchIcon);
+    this.element.append(innerHtml);
 
-      	this.label = $(` <div class="content" > </div>`);
-		this.element.prepend(this.label)
-	}
-	addSwitch(){
-		$(this.element).append(`<div class="content"><div class="ui toggle checkbox">
+    this.label = $(` <div class="content" > </div>`);
+    this.element.prepend(this.label);
+  }
+  addSwitch() {
+    $(this.element)
+      .append(`<div class="content"><div class="ui toggle checkbox">
   			<input type="checkbox" name="public">
   			<label></label>
 		</div></div>`);
-	}
-	popup(text){
-		this.label.popup({
-			content : text
-		});
+  }
+  popup(text) {
+    this.label.popup({
+      content: text
+    });
 
-		this.label.popup('show');
-		var self =this;
-		setTimeout(function(){
-			self.label.popup('hide');
-		}, 3000);
-	}
-	addColor(color){
-		this.label.css("background-color", color);
-	}
-	addRemoveIcon(callback){
-		this.removeIcon = $(`<i class=" remove icon"></i>`); 
-		this.removeIcon.on('click', function(){
-			callback();
-		});
-		this.label.append(this.removeIcon);
-	}
-	render(){
-		$('#'+ this.divToBindTo).append(this.element);
-		
-	}
-	deleteMe(){
-		this.element.remove();
-		this.deleteCourseCallback(this.coursePointer);
-	}
-	focus(){
-		this.searchBox.focus();
-	}
-	getKey(){
-		return this.inputText;
-	}
-	refreshKey(){
-		this.inputText = this.searchBox.val();
-	}
-	attachKeydownHandler(){
-		var self = this;
-		this.element.keydown(function(e) {
-			setTimeout(function() {
-				if(this.active && e.keyCode != 13){
-					this.active=false;
-					self.deleteCourseCallback(self.coursePointer);
-					
-				}
-				self.refreshKey();
-				if (e.keyCode == 13) {
-					if(!this.active){
-						this.active = self.inputText;
-						self.enterPressed(self.inputText);
-					}
-
-				}
-			}, 20);
-		});
-	}
-	attachCoursePointer(course){
-		this.coursePointer = course;
-	}
-	attachDeleteHandler(callback){
-		this.deleteCourseCallback = callback;
-	}
-	attachEnterPressHandler(callback){
-		this.enterPressed = callback;
-	}
+    this.label.popup("show");
+    var self = this;
+    setTimeout(function() {
+      self.label.popup("hide");
+    }, 3000);
+  }
+  addColor(color) {
+    this.label.css("background-color", color);
+  }
+  addRemoveIcon(callback) {
+    this.removeIcon = $(`<i class=" remove icon"></i>`);
+    this.removeIcon.on("click", function() {
+      callback();
+    });
+    this.label.append(this.removeIcon);
+  }
+  render() {
+    $("#" + this.divToBindTo).append(this.element);
+  }
+  deleteMe() {
+    this.element.remove();
+    this.deleteCourseCallback(this.coursePointer);
+  }
+  focus() {
+    this.searchBox.focus();
+  }
+  getKey() {
+    return this.inputText;
+  }
+  refreshKey() {
+    this.inputText = this.searchBox.val();
+  }
+  attachKeydownHandler() {
+    var self = this;
+    this.element.keydown(function(e) {
+      setTimeout(function() {
+        if (this.active && e.keyCode != 13) {
+          this.active = false;
+          self.deleteCourseCallback(self.coursePointer);
+        }
+        self.refreshKey();
+        if (e.keyCode == 13) {
+          if (!this.active) {
+            this.active = self.inputText;
+            self.enterPressed(self.inputText);
+          }
+        }
+      }, 20);
+    });
+  }
+  attachCoursePointer(course) {
+    this.coursePointer = course;
+  }
+  attachDeleteHandler(callback) {
+    this.deleteCourseCallback = callback;
+  }
+  attachEnterPressHandler(callback) {
+    this.enterPressed = callback;
+  }
 }
