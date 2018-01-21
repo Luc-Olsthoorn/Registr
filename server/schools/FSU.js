81ea8305faa641acaa0ba9c25bf11b488c936282
Luc-Olsthoorn <lucols97@gmail.com>
2018-01-20 23:04:21 -0500

    delaying my merge but pushing to git

---
+++ server/schools/FSU.js
@@ -0,0 +1,19 @@
+const request = require("request");
+
+class FSU {
+  constructor() {}
+  getCoursePeriods(req, res) {}
+  getCourseInfo(req, res) {
+    let url = "https://fsusucks.com/wegoodtho";
+
+    console.log(url);
+    request(url, function(error, response, html) {
+      if (!error) {
+        console.log("got it");
+        res.send(html);
+      }
+    });
+  }
+}
+
+module.exports = new FSU();
