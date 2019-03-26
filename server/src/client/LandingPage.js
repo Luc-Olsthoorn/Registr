c87fd55269b7a74c859667f065c21a6eb6551452
Luc Olsthoorn <lucols97@gmail.com>
2019-03-26 08:53:42 -0400

    added bookmarking

--- server/src/client/LandingPage.js
+++ server/src/client/LandingPage.js
@@ -45,6 +45,7 @@ class App extends React.Component {
          <Grid item xs={4}>
             <div style={{paddingBottom:"40%"}}>
             <Typography variant="h4" gutterBottom color="textSecondary">Whats new?</Typography>
+                <Typography variant="body1" color="textSecondary" >2.0.4 Added bookmarking</Typography> 
                 <Typography variant="body1" color="textSecondary" >2.0.3 Can't have duplicate days</Typography> 
                 <Typography variant="body1" color="textSecondary" >2.0.2 Filter out web classes</Typography> 
                 <Typography variant="body1" color="textSecondary" >2.0.1 Web class support</Typography> 
