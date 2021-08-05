17df24e3b571921d0b4c20a250c0d080afa358f1
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2021-08-05 04:53:30 +0000

    dark mode switch

--- server/src/client/App.js
+++ server/src/client/App.js
@@ -1,50 +1,71 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles, ThemeProvider, createTheme } from '@material-ui/core/styles';
+import { Switch, CssBaseline } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
 
-
-import { SideMenu } from './SideMenu.js'; 
+import { SideMenu } from './SideMenu.js';
 import { Home } from './Home.js';
 
-const styles = theme => ({
+import logo_light from './assets/logo.png';
+import logo_dark from './assets/logo_dark.png';
+
+import Brightness5Icon from '@material-ui/icons/Brightness5';
+import Brightness2Icon from '@material-ui/icons/Brightness2';
+
+const useStyles = makeStyles(theme => ({
   content: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   mainView: {
-    overflow:'hidden',
+    overflow: 'hidden',
     display: 'flex',
     [theme.breakpoints.down('xs')]: {
       display: 'inherit'
     }
   }
-  
+}));
 
+const darkTheme = createTheme({
+  palette: { type: 'dark' },
+  logo: logo_light
 });
-class Main extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
-  render(){
+const lightTheme = createTheme({
+  palette: { type: 'light' },
+  logo: logo_dark
+});
 
-    return (
-      <div className={this.props.classes.mainView}>
-        <SideMenu/>
-        <main className={this.props.classes.content}>
-          <Home/>
 
-        </main>
-       
-      </div>
-    )
-  }
+function main() {
+  // App-wide dark mode toggle
+  const [darkMode, setDarkMode] = useState(false);
+
+  // Styling hook
+  const classes = useStyles();
+
+  return (
+    
+      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+        <CssBaseline/>
+        <div className={classes.mainView}>
+          <SideMenu />
+          <main className={classes.content}>
+            <div style={{ paddingLeft: '16px' }}>
+              {/* <span> */}
+                <Brightness5Icon style={{verticalAlign: 'middle'}}/>
+                <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
+                <Brightness2Icon style={{verticalAlign: 'middle'}}/>
+              {/* </span> */}
+            </div>
+              <Home/>
+          </main>
+        </div>
+      </ThemeProvider>
+
+  )
 }
 
-Main.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-const MainWithStyles = withStyles(styles)(Main);
-const App = connect()(MainWithStyles);
+const App = connect()(main);
 
-export { App }
+export { App };
--- server/src/client/Home.js
+++ server/src/client/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
@@ -11,67 +11,64 @@ import Switch from '@material-ui/core/Switch';
 
 import InfiniteScroll from 'react-infinite-scroller';
 
-import {Calendar} from './Calendar.js';
-import {LandingPage} from './LandingPage.js';
+import { Calendar } from './Calendar.js';
+import { LandingPage } from './LandingPage.js';
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
   root: {
     paddingTop: theme.spacing.unit * 4,
-    paddingLeft: theme.spacing.unit * 1,
-    paddingRight: theme.spacing.unit * 1,
+    paddingLeft: theme.spacing.unit * 2, // Account for now-missing 8px <body> margin
+    paddingRight: theme.spacing.unit * 2,
   }
-});
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+}));
+
+function HomeComponent(props) {
+  const classes = useStyles();
+
+  if (props.numFiltered == props.totalCalendars && props.totalCalendars != 0) {
+    return (<Typography variant="h2" color="textSecondary" gutterBottom style={{ textAlign: "center", paddingTop: 100, paddingBottom: 50 }}>
+      OOPS YA FILTERED EVERYTHING</Typography>)
   }
+  else if (props.calendars.length > 0) {
+    return (
 
-  render(){
-
-    if (this.props.numFiltered == this.props.totalCalendars && this.props.totalCalendars!=0){
-      return (<Typography variant="h2" color="textSecondary" gutterBottom style={{textAlign:"center", paddingTop:100, paddingBottom:50}}>
-OOPS YA FILTERED EVERYTHING</Typography>)
-    }
-    else if(this.props.calendars.length>0){
-      return (
-        
-        <div className={this.props.classes.root}>
-        <FormControlLabel control={<Switch 
-          value={this.props.bookMarkOnly}
-          onChange={()=>this.props.dispatch({type:"TOGGLE_BOOK_MARK", data:!this.props.bookMarkOnly})}
-            />} label="Show only Bookmarked" />
+      // <div className={classes.root}>
+      <div className={classes.root}>
+        <FormControlLabel control={<Switch
+          value={props.bookMarkOnly}
+          onChange={() => props.dispatch({ type: "TOGGLE_BOOK_MARK", data: !props.bookMarkOnly })}
+        />} label="Show only Bookmarked" />
         <InfiniteScroll
-              pageStart={0}
-              loadMore={()=>this.props.dispatch({type:"INCREASE_NUM_CALENDARS"})}
-              hasMore={this.props.hasMore}
-              loader={<div key={0}>Loading ...</div>}
-            >
-          {this.props.calendars.map((Calendardata, key)=><Calendar calendar={Calendardata} key={key}/>)}
-          </InfiniteScroll>
-        </div>
+          pageStart={0}
+          loadMore={() => props.dispatch({ type: "INCREASE_NUM_CALENDARS" })}
+          hasMore={props.hasMore}
+          loader={<div key={0}>Loading ...</div>}
+        >
+          {props.calendars.map((Calendardata, key) => <Calendar calendar={Calendardata} key={key} />)}
+        </InfiniteScroll>
+      </div>
 
-      )
-    }else{
-      return(
-        <LandingPage/>
-        );
-    }
+    )
+  } else {
+    return (
+      <LandingPage />
+    );
   }
 }
+
 const mapStateToProps = (state) => {
   return {
-    calendars: state.filteredCalendars.slice(0,state.numCalendarsToDisplay),
-    hasMore: (!state.bookMarkOnly && (state.numCalendarsToDisplay <= state.filteredCalendars.length) ) || (state.bookMarkOnly&&(state.numCalendarsToDisplay <= state.numBookmarked )),
+    calendars: state.filteredCalendars.slice(0, state.numCalendarsToDisplay),
+    hasMore: (!state.bookMarkOnly && (state.numCalendarsToDisplay <= state.filteredCalendars.length)) || (state.bookMarkOnly && (state.numCalendarsToDisplay <= state.numBookmarked)),
     numFiltered: state.numFiltered,
     totalCalendars: state.calendars.length,
     bookMarkOnly: state.bookMarkOnly,
 
   }
 }
-App.propTypes = {
+HomeComponent.propTypes = {
   classes: PropTypes.object.isRequired,
 };
-const AppWithStyles = withStyles(styles)(App);
-const Home = connect(mapStateToProps, null)(AppWithStyles);
+const Home = connect(mapStateToProps, null)(HomeComponent);
 
 export { Home }
