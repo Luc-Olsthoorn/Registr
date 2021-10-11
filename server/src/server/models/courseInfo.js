d32cc5d5d4305eb7b56b7555cf9d19409cd9440d
Muhamed Hobi <woohoomoo2u@gmail.com>
2021-10-11 12:23:21 -0400

    Cleanup

--- server/src/server/models/courseInfo.js
+++
@@ -1,5 +0,0 @@
-const Course = require("./course").Course;
-const Section = require("./section").Section;
-const MeetingTime = require("./meetingTime").MeetingTime;
-
-module.exports.Course = Course;
