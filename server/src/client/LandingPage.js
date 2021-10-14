2e802414f7c5330c82e9cc68a408839b109e0a7a
Luc Olsthoorn <lucols97@gmail.com>
2021-10-13 20:02:08 -0500

    Update LandingPage.js

--- server/src/client/LandingPage.js
+++ server/src/client/LandingPage.js
@@ -93,6 +93,7 @@ class App extends React.Component {
          <Grid item xs={6}>
             <div style={{paddingBottom:"40%"}}>
                 <Typography variant="h4" gutterBottom color="textSecondary">Whats new?</Typography>
+                <Typography variant="body1" color="textSecondary" >2.1.5 Spring '22 🗿</Typography>
                 <Typography variant="body1" color="textSecondary" >2.1.4 Fall/Summer 2021 😩</Typography>
                 <Typography variant="body1" color="textSecondary" >2.1.3 Fall/Summer 2020 baby (share this page to see daddy)</Typography>
                 <Typography variant="body1" color="textSecondary" >2.1.2 Spring 2020 baby</Typography>
